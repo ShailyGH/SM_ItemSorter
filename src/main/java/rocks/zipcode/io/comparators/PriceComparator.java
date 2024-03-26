3f11eed0624d2baf9374773fdff80ecf45332743
Shaily Modi <shailykunal@gmail.com>
2024-03-26 13:57:28 -0400

    PriceComparator class completed

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,8 +1,28 @@
 package rocks.zipcode.io.comparators;
 
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item>
+{
+    @Override
+    public int compare(Item o1, Item o2)
+    {
+        if (o1.getPrice() > o2.getPrice())
+        {
+            return 1;
+        }
+        else if (Objects.equals(o1.getPrice(), o2.getPrice()))
+        {
+            return 0;
+        }
+        return -1;
+    }
 }
+
